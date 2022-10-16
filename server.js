63cf9ca6004c29108ef9871c46a389a39a3fb0ec
loudagnese <louis.dagnese@gmail.com>
2022-10-16 11:11:51 -0700

    gets all notes on note page

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const notes = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -7,6 +8,10 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -17,7 +22,7 @@ app.get("/notes", (req, res) => {
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT} - http://localhost:${PORT}`));
